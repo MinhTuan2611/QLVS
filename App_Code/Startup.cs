28c498b217b71e464a115cf9887b67157708cb07
Binh Nguyen <quangbinh136@gmail.com>
2018-10-09 11:38:07 +0700

    init project

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Quan_li_ve_so.Startup))]
+namespace Quan_li_ve_so
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
